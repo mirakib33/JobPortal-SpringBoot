a7001ec3a02ebcefb8c497f5d97ab932d2691037
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-01-21 12:47:31 +0600

    file upload success

--- JobPortal/src/main/java/com/misoft/jobportal/DTO/ResponseMessage.java
+++ JobPortal/src/main/java/com/misoft/jobportal/DTO/ResponseMessage.java
@@ -2,4 +2,10 @@ package com.misoft.jobportal.DTO;
 
 public class ResponseMessage {
 	public String message;
+
+	public ResponseMessage(String message) {
+		super();
+		this.message = message;
+	}
+	
 }
