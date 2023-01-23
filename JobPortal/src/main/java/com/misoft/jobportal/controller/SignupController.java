2b033dc8beadf5cf9660013a8d6be41619bddc93
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-01-23 13:52:12 +0600

    signup form data submitted

--- JobPortal/src/main/java/com/misoft/jobportal/controller/SignupController.java
+++ JobPortal/src/main/java/com/misoft/jobportal/controller/SignupController.java
@@ -1,10 +1,9 @@
 package com.misoft.jobportal.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.misoft.jobportal.entity.Signup;
@@ -17,8 +16,8 @@ public class SignupController {
 	@Autowired
 	SignupRepository signupRepository;
 	
-	@GetMapping("/signup")
-    List<Signup> all() {
-        return signupRepository.findAll();
+	@PostMapping("/signup")
+    Signup post(@RequestBody Signup signup) {
+        return signupRepository.save(signup);
     }
 }
