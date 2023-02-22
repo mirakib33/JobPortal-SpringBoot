ae840cfd16220184a723e39415d222deee47cf2f
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-22 23:31:21 +0600

    Revert "Revert "Revert "login by session"""
    This reverts commit aa42f90d98e2f47ffbfbc62b577185741be17501.

--- JobPortal/src/main/java/com/misoft/jobportal/controller/LoginController.java
+++ JobPortal/src/main/java/com/misoft/jobportal/controller/LoginController.java
@@ -1,35 +1,5 @@
 package com.misoft.jobportal.controller;
 
-import com.misoft.jobportal.entity.Signup;
-import com.misoft.jobportal.service.employer.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class LoginController {
-
-    @Autowired
-    private LoginService loginService;
-
-    @PostMapping("/login")
-    public ResponseEntity<Signup> login(@RequestBody Signup signup, HttpSession session) {
-        Signup authenticatedUser = loginService.authenticate(signup.getEmail(), signup.getPassword());
-        if (authenticatedUser != null) {
-            session.setAttribute("signup", authenticatedUser);
-            return ResponseEntity.ok(authenticatedUser);
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    @GetMapping("/logout")
-    public ResponseEntity<Void> logout(HttpSession session) {
-        session.invalidate();
-        return ResponseEntity.ok().build();
-    }
 
 }
--- JobPortal/src/main/java/com/misoft/jobportal/controller/SignupController.java
+++ JobPortal/src/main/java/com/misoft/jobportal/controller/SignupController.java
@@ -1,8 +1,6 @@
 package com.misoft.jobportal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +18,7 @@ public class SignupController {
 	
 	@PostMapping("/signup")
 	public Signup save(@RequestBody Signup signup) {
-		if (signupService.findByEmail(signup.getEmail()) != null) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).build();
-		}
-
-		// Set the role to "Applicant" by default
-//		user.setRole("Applicant");
-
-		Signup savedUser = signupService.save(signup);
-		return ResponseEntity.ok(message);
-	}
-		return signupService.save(signup);
+		signupService.save(signup);
+		return signup;
 	}
 }
